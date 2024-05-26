6308e7936da35025210f8313d1e3a5dec9029a1c
Dikshant Aggarwal <ce22b052@gmail.com>
2024-05-26 11:45:41 +0530

    loadingUiChanged

--- src/components/LoadingPage.js
+++ src/components/LoadingPage.js
@@ -1,16 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import '../css/Loading.css'; // Import CSS file for styling
+import logo from "../assets/973300f3-c585-48d9-9e8c-601a3ae24121.png"
 
 function LoadingPage() {
-  
+
   return (
-    <div className="loading-page-container">
-      
-        <div className="loading-spinner-container">
-          <div className="loading-spinner"></div>
-          <div className="loading-text">Loading...</div>
-        </div>
-      
+    <div className="loading-cont">
+      <img className="logo" src={logo} alt="Logo" />
     </div>
   );
 }
