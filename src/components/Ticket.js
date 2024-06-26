8479f37d21f6e1a80524cb0a98e06add2c3df5c7
DIKSHANTAGGARWAL <125082477+DIKSHANTAGGARWAL@users.noreply.github.com>
2024-06-26 22:45:28 +0530

    alerting message after ticket

--- src/components/Ticket.js
+++ src/components/Ticket.js
@@ -31,9 +31,11 @@ function Ticket() {
   const TicketToast = () => toast.promise(Submit(), {
     loading: 'Raising Ticket',
     success: (result) => {
+      alert(result.message)
         return result.message;
     },
     error: (result) => {
+      alert(result.message)
         return result.message
     },
 },{
