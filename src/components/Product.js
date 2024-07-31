0b4769bbd73163296564cd48ba52fd0a11f890ee
instibuzz <jujhar.channa@gmail.com>
2024-08-01 02:45:23 +0530

    mobile view sorted

--- src/components/Product.js
+++ src/components/Product.js
@@ -337,7 +337,7 @@ const Product = () => {
                       }
                     }
                   }}
-                  itemsToShow={4.5}
+                  itemsToShow={4}
                   speed={300}
                 // centerMode
                 >
